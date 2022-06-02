8b2cab1faf6d71b0b4ed4c6b832daf5edd9e722f
Anatoliy Poletaev <shtepser@yandex.ru>
2022-06-02 23:20:19 +0300

    Реализован первый ход пешки на две клетки

--- pieces/pawn.py
+++ pieces/pawn.py
@@ -6,16 +6,18 @@ from utils import on_same_file
 class Pawn(Piece):
 
     def _can_move(self, to_position):
-        if not on_same_file(self.position, to_position):
+        if not on_same_file(self.position,
+                            to_position)\
+                or not self._board.exists_free_route(self.position,
+                                                     to_position):
             return False
         to_rank = int(to_position[1])
-        if to_rank == self.rank - 1 and 0 <= to_rank <= 7\
-                and self._colour == Colour.BLACK:
-            return True
-        if to_rank == self.rank + 1 and 0 <= to_rank <= 7\
-                and self._colour == Colour.WHITE:
-            return True
-        return False
+        possible_distance = 1 if self.already_moved else 2
+        if self._colour == Colour.WHITE:
+            return 1 <= (to_rank - self.rank) <= possible_distance \
+                    and 0 <= to_rank <= 7
+        return 1 <= (self.rank - to_rank) <= possible_distance \
+                and 0 <= to_rank <= 7\
 
     @property
     def symbol(self):
