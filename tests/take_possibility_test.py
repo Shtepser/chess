49c96074cce185890212d5d6589b8677d1e607a1
Anatoliy Poletaev <shtepser@yandex.ru>
2022-06-18 23:13:20 +0300

    Завершены тесты на взятие фигур

--- tests/take_possibility_test.py
+++ tests/take_possibility_test.py
@@ -75,75 +75,95 @@ def test_knight_possible_takes(position: str,
     _assert_possible_takes_equals(Knight, position, another_pieces, possible_takes)
 
 
-# @pytest.mark.parametrize("position,colour,already_moved,"
-#                          "another_pieces,possible_moves", [
-#                              ("E2", Colour.WHITE, False,
-#                               [],
-#                               {"E3", "E4"}),
-#                              ("E3", Colour.WHITE, True,
-#                               [],
-#                               {"E4"}),
-#                              ("E3", Colour.WHITE, True,
-#                               [(Pawn, Colour.BLACK, "E4"),
-#                                (Pawn, Colour.BLACK, "F4")],
-#                               {}),
-#                              ("E2", Colour.WHITE, False,
-#                               [(Pawn, Colour.BLACK, "E3")],
-#                               {}),
-#                              ("C7", Colour.BLACK, False,
-#                               [],
-#                               {"C6", "C5"})
-#                          ])
-# def test_pawn_possible_takes(position: str, colour: Colour, already_moved: bool,
-#                        another_pieces: Iterable[Tuple[type, Colour, str]],
-#                        possible_moves: Iterable[str]):
-#     board = Board()
-#     board.add_piece(position, Pawn, colour)
-#     for type_, colour, pos in another_pieces:
-#         board.add_piece(pos, type_, colour)
-#     pawn = board[position]
-#     if already_moved:
-#         pawn._already_moved = True
-#     assert set(pawn.possible_ordinary_moves()) == set(possible_moves)
-# 
-# 
-# @pytest.mark.parametrize("position,another_pieces,possible_moves", [
-#     ("A1",
-#      [],
-#      {"A2", "A3", "A4", "A5", "A6", "A7", "A8",
-#       "B1", "C1", "D1", "E1", "F1", "G1", "H1",
-#       "B2", "C3", "D4", "E5", "F6", "G7", "H8"}),
-#     ("E4",
-#      [(Pawn, Colour.WHITE, "E3"), (Rook, Colour.WHITE, "C4"),
-#       (Pawn, Colour.WHITE, "C2")],
-#      {"E5", "E6", "E7", "E8", "F4", "G4", "H4", "D4",
-#       "D3", "F5", "G6", "H7", "F3", "G2", "H1",
-#       "D5", "C6", "B7", "A8"})
-# ])
-# def test_queen_possible_takes(position: str,
-#                         another_pieces: Iterable[Tuple[type, Colour, str]],
-#                         possible_moves: Iterable[str]):
-#     _assert_possible_takes_equals(Queen, position, another_pieces, possible_moves)
-# 
-# 
-# @pytest.mark.parametrize("position,another_pieces,possible_moves", [
-#     ("A1",
-#      [],
-#      {"A2", "A3", "A4", "A5", "A6", "A7", "A8",
-#       "B1", "C1", "D1", "E1", "F1", "G1", "H1"}),
-#     ("A1",
-#      [(Rook, Colour.WHITE, "F1")],
-#      {"A2", "A3", "A4", "A5", "A6", "A7", "A8",
-#       "B1", "C1", "D1", "E1"}),
-#     ("E4",
-#      [(Pawn, Colour.WHITE, "E3"), (Rook, Colour.WHITE, "C4")],
-#      {"E5", "E6", "E7", "E8", "F4", "G4", "H4", "D4"})
-# ])
-# def test_rook_possible_takes(position: str,
-#                        another_pieces: Iterable[Tuple[type, Colour, str]],
-#                        possible_moves: Iterable[str]):
-#     _assert_possible_takes_equals(Rook, position, another_pieces, possible_moves)
-# 
+@pytest.mark.parametrize("position,colour,another_pieces,possible_takes", [
+                             ("E2", Colour.WHITE,
+                              [],
+                              {}),
+                             ("F5", Colour.BLACK,
+                              [],
+                              {}),
+                             ("E2", Colour.WHITE,
+                              [(Pawn, Colour.BLACK, "E3")],
+                              {}),
+                             ("E2", Colour.WHITE,
+                              [(Pawn, Colour.BLACK, "F4")],
+                              {}),
+                             ("E3", Colour.WHITE,
+                              [(Pawn, Colour.BLACK, "F4")],
+                              {"F4"}),
+                             ("E7", Colour.BLACK,
+                              [(Pawn, Colour.WHITE, "F6")],
+                              {"F6"}),
+                             ("E7", Colour.WHITE,
+                              [(Pawn, Colour.BLACK, "F6")],
+                              {}),
+                             ("E3", Colour.WHITE,
+                              [(Pawn, Colour.BLACK, "E4"),
+                               (Pawn, Colour.BLACK, "F4")],
+                              {"F4"}),
+                             ("E3", Colour.WHITE,
+                              [(Pawn, Colour.BLACK, "F4"),
+                               (Pawn, Colour.BLACK, "D4")],
+                              {"F4", "D4"}),
+                             ("E3", Colour.WHITE,
+                              [(Pawn, Colour.BLACK, "E4"),
+                               (Pawn, Colour.BLACK, "F4"),
+                               (Pawn, Colour.BLACK, "D4")],
+                              {"F4", "D4"}),
+                         ])
+def test_pawn_possible_takes(position: str, colour: Colour,
+                       another_pieces: Iterable[Tuple[type, Colour, str]],
+                       possible_takes: Iterable[str]):
+    board = Board()
+    board.add_piece(position, Pawn, colour)
+    for type_, colour, pos in another_pieces:
+        board.add_piece(pos, type_, colour)
+    pawn = board[position]
+    assert set(pawn.possible_takes()) == set(possible_takes)
+
+
+@pytest.mark.parametrize("position,another_pieces,possible_takes", [
+    ("A1",
+     [],
+     {}),
+    ("E4",
+     [(Pawn, Colour.BLACK, "E3"), (Rook, Colour.BLACK, "C4"),
+      (Pawn, Colour.WHITE, "C2")],
+     {"C4", "E3"}),
+    ("E4",
+     [(Pawn, Colour.WHITE, "E3"), (Rook, Colour.WHITE, "C4"),
+      (Pawn, Colour.BLACK, "C2")],
+     {"C2"})
+])
+def test_queen_possible_takes(position: str,
+                        another_pieces: Iterable[Tuple[type, Colour, str]],
+                        possible_takes: Iterable[str]):
+    _assert_possible_takes_equals(Queen, position, another_pieces, possible_takes)
+
+
+@pytest.mark.parametrize("position,another_pieces,possible_takes", [
+    ("E4",
+     [],
+     {}),
+    ("E4",
+     [(Rook, Colour.BLACK, "H4")],
+     {"H4"}),
+    ("E4",
+     [(Rook, Colour.BLACK, "H4"), (Rook, Colour.WHITE, "G4")],
+     {}),
+    ("E4",
+     [(Rook, Colour.BLACK, "E8")],
+     {"E8"}),
+    ("E4",
+     [(Rook, Colour.BLACK, "D2"), (Rook, Colour.BLACK, "G7"),
+      (Rook, Colour.BLACK, "G6")],
+     {}),
+])
+def test_rook_possible_takes(position: str,
+                       another_pieces: Iterable[Tuple[type, Colour, str]],
+                       possible_takes: Iterable[str]):
+    _assert_possible_takes_equals(Rook, position, another_pieces, possible_takes)
+
 
 def _assert_possible_takes_equals(piece_type, position: str,
                                   another_pieces, possible_takes):
@@ -153,3 +173,4 @@ def _assert_possible_takes_equals(piece_type, position: str,
         board.add_piece(pos, type_, colour)
     piece = board[position]
     assert set(piece.possible_takes()) == set(possible_takes)
+
